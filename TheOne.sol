d3d53c2ea126c8f16871a0172a62ac3aafb7ef28
Westindiess <gregoryalexiss@gmail.com>
2021-05-11 16:47:19 +0200

    Sol

---
+++ TheOne.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TheOne {
+    uint256 num = 1;
+
+    function one() public view returns (uint256) {
+        return num;
+    }
+}
